c84aa8b1b8b838f022d21dc1a386bf5b9e6bcc93
Sathish <jobad.database@gmail.com>
2026-06-27 13:22:01 +0530

    Update sitemap.js

--- api/sitemap.js
+++ api/sitemap.js
@@ -6,22 +6,11 @@ const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default async function handler(req, res) {
-  // Fetch all active jobs
-  const { data: jobs, error } = await supabase
-    .from('active_jobs')
-    .select('job_id, posted_date, expiry_date')
-    .order('posted_date', { ascending: false });
-
-  if (error || !jobs) {
-    res.status(500).send('Error generating sitemap');
-    return;
-  }
-
   const baseUrl = 'https://jobad-pwa.vercel.app';
   const now = new Date().toISOString();
 
-  // Generate sitemap XML
-  let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  // Return sitemap index
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <sitemap>
     <loc>${baseUrl}/sitemap-jobs.xml</loc>
@@ -29,32 +18,6 @@ export default async function handler(req, res) {
   </sitemap>
 </sitemapindex>`;
 
-  // Also generate individual job sitemap
-  let jobSitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-    <loc>${baseUrl}</loc>
-    <lastmod>${now}</lastmod>
-    <changefreq>daily</changefreq>
-    <priority>1.0</priority>
-  </url>`;
-
-  jobs.forEach(job => {
-    const lastmod = job.posted_date || now;
-    jobSitemap += `
-  <url>
-    <loc>${baseUrl}/job/${job.job_id}</loc>
-    <lastmod>${lastmod}</lastmod>
-    <changefreq>weekly</changefreq>
-    <priority>0.8</priority>
-  </url>`;
-  });
-
-  jobSitemap += `
-</urlset>`;
-
-  // Return both sitemaps
-  // For the main sitemap.xml, return the index
   res.setHeader('Content-Type', 'application/xml');
   res.status(200).send(sitemap);
 }
