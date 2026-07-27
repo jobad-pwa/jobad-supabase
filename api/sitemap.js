4c8b7c94dc37aff7bc282a140c334052d1680368
Sathish <jobad.database@gmail.com>
2026-07-27 17:57:43 +0530

    Update sitemap.js

--- api/sitemap.js
+++ api/sitemap.js
@@ -1,16 +1,31 @@
-// api/sitemap.js - SIMPLIFIED TEST VERSION
+// api/data/sitemap.js
 export default function handler(req, res) {
   const baseUrl = 'https://jobad-pwa.vercel.app';
-  const now = new Date().toISOString();
-
+  const now = new Date().toISOString().split('T')[0];
+  
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <sitemap>
-    <loc>${baseUrl}/sitemap-jobs.xml</loc>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>${baseUrl}</loc>
     <lastmod>${now}</lastmod>
-  </sitemap>
-</sitemapindex>`;
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>${baseUrl}/api/data/job-list</loc>
+    <lastmod>${now}</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>0.9</priority>
+  </url>
+  <url>
+    <loc>${baseUrl}/api/data/indeed-feed</loc>
+    <lastmod>${now}</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>0.8</priority>
+  </url>
+</urlset>`;
 
   res.setHeader('Content-Type', 'application/xml');
+  res.setHeader('Cache-Control', 'public, max-age=86400');
   res.status(200).send(sitemap);
 }
