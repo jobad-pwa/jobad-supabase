32462251165c94cfb3891ebb99c6b261e93701f4
Sathish <jobad.database@gmail.com>
2026-07-27 17:56:33 +0530

    Update robots.js

--- api/robots.js
+++ api/robots.js
@@ -4,23 +4,35 @@ export default function handler(req, res) {
 User-agent: *
 Allow: /
 Allow: /job/
-Allow: /sitemap.xml
-Allow: /sitemap-jobs.xml
-Disallow: /api/
+Allow: /api/data/job-list
+Allow: /api/data/job-id
 Disallow: /profile/
 Disallow: /edit-seeker/
 Disallow: /postjob/
 
+# Google Jobs
+User-agent: Googlebot
+Allow: /job/
+Allow: /api/data/job-id
+Allow: /api/data/job-list
+
+# Indeed
+User-agent: Indeedbot
+Allow: /job/
+Allow: /api/data/indeed-feed
+
+# Sitemaps
+Sitemap: https://jobad-pwa.vercel.app/api/data/sitemap-jobs
 Sitemap: https://jobad-pwa.vercel.app/sitemap.xml
 
-# Crawl delay for Googlebot
+# Crawl delay
 User-agent: Googlebot
 Crawl-delay: 1
 
-# Crawl delay for Bingbot
 User-agent: Bingbot
 Crawl-delay: 1`;
 
   res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Cache-Control', 'public, max-age=86400');
   res.status(200).send(robots);
 }
